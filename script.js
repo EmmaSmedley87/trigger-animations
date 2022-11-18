6884f1da14670651e0198ba304661512e43748c3
EmmaSmedley87 <EmmaLondon1987@hotmail.com>
2022-11-18 18:14:41 +0000

    Explosion class added

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 700;
+const explosions = [];
 
-ctx.fillStyle = "white";
-ctx.fillRect(50, 50, 100, 150);
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    this.image = new Image();
+    this.image.src = "images/boom.png";
+    this.frame = 0;
+  }
+}
+
+//
